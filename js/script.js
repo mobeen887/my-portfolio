d017c3305bc3d2327750097765db12834badab8f
Mobeen Shah <41565155+mobeen887@users.noreply.github.com>
2023-09-29 04:59:43 +0500

    Portfolio Base Structure is created.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Typing Script Animation
+
+var typed = new Typed(".typing", {
+    strings:["Web Developer", "Web Designer", "Graphic Designer", "Digital Marketing Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
